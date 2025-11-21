40d30de6032bc6de815295afef89ed1583ce6bf3
sjysngh <sujoy@wrytrsblck.com>
2025-11-21 12:34:21 +0530

    adds runs-on

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,14 @@
+// package auth
+
+// import (
+// 	"net/http"
+// 	"testing"
+// )
+
+// func TestGetAPIKey(headers http.Header, t *testing.T) {
+// 	got, err := GetAPIKey(headers)
+// 	if err != nil {
+// 		t.Error(err)
+// 	}
+
+// }
